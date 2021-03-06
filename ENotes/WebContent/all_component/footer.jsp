e1a5bc75eafdb503bcb5862d809e076e33f5f69d
SnehaKhetree <79041231+SnehaKhetree@users.noreply.github.com>
2021-03-06 13:03:41 +0530

    Add files via upload

---
+++ ENotes/WebContent/all_component/footer.jsp
@@ -0,0 +1,4 @@
+<div class="contaier-fluid bg-dark mt-5">
+<p class="text-center text-white">Note: Any Errors occur then contact CodeWith Sneha. Designed and developed by Sneha Khetree</p>
+<p class="text-center text-white">All Right Reserved @CodeWithSneha-2021</p>
+</div>
---
+++ ENotes/src/com/DB/DBConnect.java
@@ -0,0 +1,20 @@
+package com.DB;
+
+import java.sql.*;
+
+public class DBConnect {
+	private static Connection con;
+	public static Connection getConnection(){
+		Connection con=null;
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/enotes", "root", "root");
+		}catch(Exception e){System.out.println(e);}
+		return con;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
